47e623a876e779e788d78560bc22ba557f65dcf9
Enrique Montemayor <eemontemayor@live.com>
2019-03-05 11:01:00 -0600

    modified

--- bookmarks.js
+++ bookmarks.js
@@ -1,5 +1,25 @@
+'use strict';
+
 const bookMarks = (function(){
 
     //event handler functions
     // render functions
+
+function handleDeleteItem(){
+
+}
+
+function handleAddItem(){
+
+}
+
+function handleDetailedView(){
+
+}
+
+function handleFilter(){
+
+}
+
+
 })
