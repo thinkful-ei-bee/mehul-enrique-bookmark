71a323d573e4f06bd651f13bcbfd1f453f50cb87
Enrique Montemayor <eemontemayor@live.com>
2019-03-05 10:40:56 -0600

    added basic store structure

---
+++ STORE.js
@@ -0,0 +1,17 @@
+const STORE = (function(){
+
+    const bookMarks = [
+        { title:"", rating:"", description: "", urlLink: "",},
+        { title:"", rating:"", description: "", urlLink: "",}
+    ];
+
+    const currentView = //condensed (default) / detailed/ is adding/ filtered/ error
+
+
+
+    return {
+        bookMarks,
+        currentView,
+    }
+
+}() );
